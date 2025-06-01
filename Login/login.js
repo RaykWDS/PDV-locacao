cc559fe358586531067aa1e09584f4f39f3ac511
RaykWDS <161895658+RaykWDS@users.noreply.github.com>
2025-06-01 14:37:29 -0300

    Add files via upload

---
+++ Login/login.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('loginForm');
+const passwordInput = document.getElementById('password');
+const togglePassword = document.getElementById('togglePassword');
+
+// Mostrar/ocultar senha
+togglePassword.addEventListener('change', function () {
+    passwordInput.type = this.checked ? 'text' : 'password';
+});
+
+// Validação simples de usuário e senha
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value.trim();
+    const password = passwordInput.value.trim();
+
+    if (username === 'rosiel.ferreira' && password === 'ferreira2025') {
+        alert('Login bem-sucedido! Bem-vindo, ' + username);
+        sessionStorage.setItem('logado', 'true');
+        window.location.href = "../locacao/locacao.html"; // Redirecionamento após login bem-sucedido
+      } else {
+        alert('Usuário ou senha incorretos!');
+      }
+    });
